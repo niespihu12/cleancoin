59bd5ccd063503b36c8bcc0713bad4d64d763b23
unknown <niespihu12@gmail.com>
2025-07-16 10:42:23 -0500

    nuevo z-index

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -22,7 +22,7 @@ export default function Navbar() {
   ]
 
   return (
-    <header className="sticky top-0 z-10000000 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 z-10000">
+    <header className="sticky top-0 z-10000000 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 z-50">
       <div className="container flex h-16 items-center justify-between">
         <div className="flex items-center gap-2">
           <Sheet open={isOpen} onOpenChange={setIsOpen}>
